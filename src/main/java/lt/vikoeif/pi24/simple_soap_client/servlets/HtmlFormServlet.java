de125202b96a52893b4bd831fe34321df43342a4
kh1dr1 <cryptn4@gmail.com>
2026-05-28 13:02:12 +0300

    Remove unused imports

--- src/main/java/lt/vikoeif/pi24/simple_soap_client/servlets/HtmlFormServlet.java
+++ src/main/java/lt/vikoeif/pi24/simple_soap_client/servlets/HtmlFormServlet.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/main/java/lt/vikoeif/pi24/simple_soap_client/servlets/HtmlServlet.java
+++ src/main/java/lt/vikoeif/pi24/simple_soap_client/servlets/HtmlServlet.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.PrintWriter;
