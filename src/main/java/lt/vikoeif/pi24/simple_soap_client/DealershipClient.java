760e55099b4e1e3762667699b626348a1c6dbc26
kh1dr1 <cryptn4@gmail.com>
2026-06-04 11:44:57 +0300

    Refactor DealershipClient (SOAP client class)

--- src/main/java/lt/vikoeif/pi24/simple_soap_client/DealershipClient.java
+++ src/main/java/lt/vikoeif/pi24/simple_soap_client/DealershipClient.java
@@ -3,24 +3,37 @@ package lt.vikoeif.pi24.simple_soap_client;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 import lt.vikoeif.pi24.wsdl.*;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class DealershipClient extends WebServiceGatewaySupport {
-    public final String WS_NAMESPACE = "http://viko.lt/dealership-service/schema/";
-    public final String WS_DEALERSHIPS_PORT = WS_NAMESPACE + "DealershipsPort/";
+    public final String WS_REQUEST_URI = "http://localhost:8081/ws/dealerships";
 
+    /**
+     * Get a list of all dealerships
+     * @return GetAllDealershipsResponse
+     */
     public GetAllDealershipsResponse getAllDealerships() {
         GetAllDealershipsRequest request = new GetAllDealershipsRequest();
 
-        GetAllDealershipsResponse response = (GetAllDealershipsResponse)
+        return (GetAllDealershipsResponse)
                 getWebServiceTemplate().marshalSendAndReceive(
-                        "http://localhost:8081/ws/dealerships",
-                        request/*,
-                        new SoapActionCallback(
-                                WS_DEALERSHIPS_PORT + "getAllDealershipsRequest"
-                        )*/
+                        WS_REQUEST_URI,
+                        request
                 );
+    }
 
-        return response;
+    /**
+     * Create a dealership
+     * @param dealership the dealership object data
+     * @return AddDealershipResponse
+     */
+    public AddDealershipResponse addDealership(Dealership dealership) {
+        AddDealershipRequest request = new AddDealershipRequest();
+        request.setDealership(dealership);
+
+        return (AddDealershipResponse)
+                getWebServiceTemplate().marshalSendAndReceive(
+                        WS_REQUEST_URI,
+                        request
+                );
     }
 }
